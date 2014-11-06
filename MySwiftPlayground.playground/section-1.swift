3dadae78724099b00121032b1fcaffd4fadc4cc5
Michael <mbcrump29@gmail.com>
2014-11-06 10:53:14 -0600

    added playground

---
+++ MySwiftPlayground.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//NSString *myString = @"This is my string.";
+
+
+
+var myString = "This is my string."
+//Constants are expressed with the ‘let’ keyword
+
+
+let SomeConstant = 40
+//In this case SomeConstant is implicitly defined as an integer.
+
+
+let kSomeConstant: Int = 40
+
+//If you want to be more specific you can specify which type it is like so
+
+//With both arrays and dictionaries, they are described using brackets []
+
+
+var colorsArray = ["Blue", "Red", "Green", "Yellow"]
+
+var colorsDictionary = ["PrimaryColor":"Green", "SecondaryColor":"Red"]
