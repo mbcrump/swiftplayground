daa41b3bdb249aa961abeb929aee5c8eede670b7
Michael <mbcrump29@gmail.com>
2014-11-13 15:38:13 -0600

    updated playground

--- MySwiftPlayground.playground/section-1.swift
+++ MySwiftPlayground.playground/section-1.swift
@@ -2,25 +2,116 @@
 
 import UIKit
 
+//Objective C
 //NSString *myString = @"This is my string.";
 
+//Swift
+
+////   Variables
+//var name: String = "Michael Crump"
+//var temperature: Double = 36.5
+//var somenumber: Int = 12345
+//var visible: Bool = true
+//
+//somenumber = 54321
+
+//   Type Inference
+
+//var name = "Michael Crump"
+//var temperature = 36.5
+//var somenumber = 12345
+//var visible = true
+
+//name = "TJ Van Toll" // OK
+//temperature = 36.8 // OK 
+//somenumber1 = 54321 // OK
+//visible = false // OK
 
 
-var myString = "This is my string."
-//Constants are expressed with the ‘let’ keyword
+//   Constants are expressed with the ‘let’ keyword
+
+//let name: String = "Michael Crump"
+//let temperature: Double = 36.5
+//let somenumber: Int = 12345
+//let visible: Bool = true
+
+//name = "John Doe" // error
+//temperature = 36.8 // error 
+//somenumber = 2015 // error
+//visible = false // error
+
+//   Unicode Name
+//let 🐶: Character = "🐶"
+//let 🐮: String = "🐮"
+//let dog: Character = "dog" // error
+//let cow: String = "cow"
+//let π = 3.1415927
+//let 言語 = "Japanese"
+
+//   String Operations
+//let hello = "Hello"
+//let world = "World"
+//let greeting = hello + " " + world // "Hello World" 
+//let a = 3, b = 5
+//let result = "\(a) + \(b) = \(a + b)" // "3 + 5 = 8"
+
+//   Array and Dictionary Literals
+//   Mixed Object Type Array
+//var array = ["Tokyo", 3, true]
+
+// Typed Collection 
+//var list1: [String] = ["Ja", "En", "Fr"]
+
+// Dictionary 
+//var legs = ["cat":4, "snake":0, "dog":2]
+
+//Array Access 
+//var items = ["Ja", "En", "Fr"]
+
+// accessors 
+//let item = items[1]
+//items.insert("De", atIndex: 0)
+//items.append("It")
+//items.removeAtIndex(1)
+//items.removeLast()
+
+////   Dictionary
+//var dict = ["Ja":"Japanese", "En":"English", "Fr":"French"]
+//
+////  read access
+//let lang = dict["Ja"] // "Japanese" 
+//
+////  write access
+//dict["De"] = "German"
+//dict["Ja"] = nil
+
+//   If Statement 
+//var items = ["Ja", "En", "Fr"]
+//if items.count > 0 {
+//    // OK: some code here
+//}
+//if (items.count > 0) {
+//    // OK: parentheses are optional 
+//}
+
+//   Switch Statement 
+//var foo = 1
+//switch foo {
+//case 1, 3, 5: println("1,3,5");
+//case 2, 4, 6: println("2,4,6");
+//case 7...9: println("7,8,9");
+//case 10...100: println("10~99");
+//default: println("other");
+//}
+
+//   Loops
+//for var i = 0 ; i < 100 ; ++i { /*code*/ }
+//
+//for ch in "123" { println(ch)
+//}
+//
+//for num in 1...5 { println(num)
+//}
 
 
-let SomeConstant = 40
-//In this case SomeConstant is implicitly defined as an integer.
 
-
-let kSomeConstant: Int = 40
-
-//If you want to be more specific you can specify which type it is like so
-
-//With both arrays and dictionaries, they are described using brackets []
-
-
-var colorsArray = ["Blue", "Red", "Green", "Yellow"]
-
-var colorsDictionary = ["PrimaryColor":"Green", "SecondaryColor":"Red"]
